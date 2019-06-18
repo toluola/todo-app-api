dcd8a93e81b64bf7d31c9bd2f5d5bdb990f8311e
Tolulope Olaniyan <toluola7@gmail.com>
2019-06-18 07:19:15 +0100

    initial commit

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name { "MyString" }
+    done { false }
+    todo { nil }
+  end
+end
---
+++ spec/factories/todos.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :todo do
+    title { "MyString" }
+    created_by { "MyString" }
+  end
+end
