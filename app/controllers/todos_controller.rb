0874e771aa5fafde711b9f1af0ffcbb293d3fd2d
Tolulope Olaniyan <toluola7@gmail.com>
2019-06-18 12:38:52 +0100

    Finished Crud operations

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,39 @@
 class TodosController < ApplicationController
     before_action :set_todo, only: [:show, :update, :destroy]
     include Response
+    include ExceptionHandler
 
     def index
         @todos = Todo.all
         json_response(@todos)
     end
 
+    def create
+        @todo = Todo.create!(todo_params)
+        json_response(@todo, :created) 
+    end
+
+    def show 
+        json_response(@todo)
+    end
+
+    def update
+        @todo.update(todo_params) 
+        head :no_content
+    end
+
+    def destroy 
+        @todo.destroy
+        head :no_content
+    end
+
     private
 
     def set_todo
-        @todo = Todo.find(params[id])
+        @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+        params.permit(:title, :created_by)
     end
 end
