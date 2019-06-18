dbd08e6d69bc152023558a15813d4adf4e802d56
Tolulope Olaniyan <toluola7@gmail.com>
2019-06-18 08:30:49 +0100

    Add Get all Todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,15 @@
 class TodosController < ApplicationController
+    before_action :set_todo, only: [:show, :update, :destroy]
+    include Response
+
+    def index
+        @todos = Todo.all
+        json_response(@todos)
+    end
+
+    private
+
+    def set_todo
+        @todo = Todo.find(params[id])
+    end
 end
