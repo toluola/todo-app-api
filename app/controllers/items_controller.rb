f66af0f5bbee197eb0a99680c7daf9f31ace2599
Tolulope Olaniyan <toluola7@gmail.com>
2019-06-19 13:58:56 +0100

    finish crud operations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,42 @@
 class ItemsController < ApplicationController
+    before_action :set_todo
+    before_action :set_todo_items, only: [:show, :update, :destroy]
+    include Response
+    include ExceptionHandler
+
+    def index
+        json_response(@todo.items)
+    end
+
+    def create
+       @todos = @todo.items.create!(todo_params)
+        json_response(@todos, :created)
+    end
+
+    def show
+       json_response(@items)
+    end
+
+    def update
+        @items.update(todo_params)
+        head :no_content
+    end
+
+    def destroy
+        @items.destroy
+    end
+
+    private
+
+    def todo_params
+        params.permit(:name, :done)
+    end
+
+    def set_todo
+        @todo = Todo.find(params[:todo_id]) 
+    end
+
+    def set_todo_items 
+        @items = @todo.items.find_by!(id: params[:id]) if @todo
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,11 @@
     Todo.create(title: Faker::Lorem.word,
         created_by: Faker::Lorem.word)
     end
+
+5.times do
+    Item.create(name: 'tolulope', todo_id: 6)
+    Item.create(name: 'testimony', todo_id: 7)
+    Item.create(name: 'juwon', todo_id: 8)
+    Item.create(name: 'emmanuel', todo_id: 9)
+    Item.create(name: 'olamide', todo_id: 10)
+end
